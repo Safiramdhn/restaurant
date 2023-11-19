240915c1ed9c21eb1b4a5aeb3df444e4efdc9866
Dewi Safira Ramadhani <safiramadhani9@gmail.com>
2023-11-19 15:53:41 +0700

    [06] User development - create user model

---
+++ graphql/users/user.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+	{
+		username: { type: String, unique: true, require: true },
+		password: { type: String, require: true },
+		first_name: { type: String, default: '' },
+		last_name: { type: String, default: '' },
+		civility: { type: String, enum: ['Mr', 'Mrs'] },
+		gender: { type: String, enum: ['male', 'female'] },
+		status: { type: String, enum: ['active', 'deleted'], default: 'active' },
+		user_type_id: { type: mongoose.Schema.ObjectId, ref: 'user_types' },
+	},
+	{
+		timestamps: true,
+	}
+);
+
+module.exports = mongoose.model('users', UserSchema);
