109c9dcd796c51cd4754c739e0d7eaa908a2241c
Dewi Safira Ramadhani <safiramadhani9@gmail.com>
2023-11-26 12:46:27 +0700

    [6] User development - Remove console.log

--- graphql/userTypes/user_type.loader.js
+++ graphql/userTypes/user_type.loader.js
@@ -3,18 +3,15 @@ const DataLoader = require('dataloader');
 const UserTypeModel = require('./user_type.model');
 
 const UserTypeLoaderData = async (userTypeIds) => {
-	console.log('userTypeIds', userTypeIds);
 	let userTypes = await UserTypeModel.find({
 		status: 'active',
 		_id: { $in: userTypeIds },
 	}).lean();
-	console.log('userTypes', userTypes);
 
 	let userTypeMap = new Map();
 	userTypes.forEach((userType) => {
 		userTypeMap.set(userType._id.toString(), userType);
 	});
-	console.log('userTypes 2', userTypes);
 	return userTypeIds.map((id) => userTypeMap.get(id.toString()));
 };
 
