8bcda6fe6e72500316c964181fce2c4edb185a17
Dewi Safira Ramadhani <safiramadhani9@gmail.com>
2023-11-27 13:49:07 +0700

    [06] User development - Create loader

--- loader/index.js
+++ loader/index.js
@@ -1,9 +1,11 @@
 const { UserTypeLoader } = require('../graphql/userTypes/user_type.loader');
+const {UserLoader} = require('../graphql/users/user.loader')
 
 module.exports = {
 	loaders: () => {
 		return {
 			UserTypeLoader: UserTypeLoader(),
+			UserLoader: UserLoader()
 		};
 	},
 };
