2f380c9bb991175b4c726c75bea2e4aa92dc2d03
Dewi Safira Ramadhani <safiramadhani9@gmail.com>
2023-12-05 18:53:36 +0700

    [07] Ingredient development - Create ingredient loader

--- loader/index.js
+++ loader/index.js
@@ -1,11 +1,13 @@
 const { UserTypeLoader } = require('../graphql/userTypes/user_type.loader');
 const { UserLoader } = require('../graphql/users/user.loader');
+const {IngredientLoader} = require('../graphql/ingredients/ingredient.loader');
 
 module.exports = {
   loaders: () => {
     return {
       UserTypeLoader: UserTypeLoader(),
       UserLoader: UserLoader(),
+      IngredientLoader: IngredientLoader(),
     };
   },
 };
