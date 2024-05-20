b12a0c55184f3ae99e78de1c2efe277ce6f94153
Dewi Safira Ramadhani <safiramadhani9@gmail.com>
2024-05-20 13:18:38 +0800

    [11] User Unit Testing - Create User

--- tests/graphql/users/user_resolvers.test.js
+++ tests/graphql/users/user_resolvers.test.js
@@ -18,29 +18,36 @@ jest.mock('../../../utils/common', () => ({
 // MUTATION
 describe('CreateUser function', () => {
   let mockUserModelFindOne;
-  let mockUserModelCreate;
-  let mockEncrypt;
   let mockUserTypeModelFindOne;
+  let mockUserModelCreate;
 
-  beforeEach(async () => {
-    jest.clearAllMocks();
-    const mongoURI = 'mongodb://' + process.env.DB_TESTING_HOST + '/' + process.env.DB_TESTING_NAME;
+  // connect once to database for testing
+  beforeAll(async () => {
+    const mongoURI = `mongodb://${process.env.DB_TESTING_HOST}/${process.env.DB_TESTING_NAME}`;
     await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 10000, // Increase timeout
-      socketTimeoutMS: 45000, // Optional: increase socket timeout
+      serverSelectionTimeoutMS: 10000,
+      socketTimeoutMS: 45000,
     });
-
-    mockUserModelFindOne = jest.spyOn(UserModel, 'findOne');
-    mockUserModelCreate = jest.spyOn(UserModel, 'create');
-    mockUserTypeModelFindOne = jest.spyOn(UserTypeModel, 'findOne');
   });
 
-  afterEach(async () => {
+  // close database connection
+  afterAll(async () => {
     await mongoose.connection.close();
-    mockUserModelFindOne.mockReset()
   });
+
+  // initiate before run each testing
+  beforeEach(() => {
+    // clear mock
+    jest.clearAllMocks();
+    
+    // define mock for database
+    mockUserModelFindOne = jest.spyOn(UserModel, 'findOne');
+    mockUserTypeModelFindOne = jest.spyOn(UserTypeModel, 'findOne');
+    mockUserModelCreate = jest.spyOn(UserModel, 'create');
+  });
+
   let userInput = {
     first_name: 'Test',
     last_name: 'User',
@@ -49,22 +56,30 @@ describe('CreateUser function', () => {
     user_type: new ObjectId('6534a9a756a7ca5ac33c58a2'),
   };
 
-  it('Should create a user with valid input', async () => {    
-    // Mock UserModel.findOne to return null (no existing user type)
+  it('Should create a user with valid input', async () => {
+    // user model find one return null because no existed user
+    mockUserModelFindOne.mockImplementation(() => {
+      return {
+        lean: jest.fn().mockResolvedValue(null)
+      }
+    });
+
+    // find user type once
     mockUserTypeModelFindOne.mockImplementationOnce();
+    // user model create return user object
+    mockUserModelCreate.mockResolvedValue({
+      ...userInput,
+      _id: new ObjectId(),
+    });
 
-    // Mock UserModel.create to be a spy
-    const mockUserModelCreate = jest.spyOn(UserModel, 'create');
-
-    // Call the Mutation.CreateUser function
+    // call create user function
     const createUserResult = await Mutation.CreateUser(null, { user_input: userInput });
 
-    // Assert that the correct functions were called with the correct arguments
+    // declare expectation
     expect(mockUserModelFindOne).toHaveBeenCalledTimes(1);
-    expect(mockUserTypeModelFindOne).toHaveBeenCalled();
+    expect(mockUserTypeModelFindOne).toHaveBeenCalledTimes(1);
     expect(mockUserModelCreate).toHaveBeenCalledTimes(1);
 
-    // Assert that the returned user is the same as the input
     expect(createUserResult.username).toEqual(userInput.username);
     expect(createUserResult.first_name).toEqual(userInput.first_name);
     expect(createUserResult.last_name).toEqual(userInput.last_name);
@@ -72,30 +87,51 @@ describe('CreateUser function', () => {
   });
 
   it('Should throw error for missing input username and password', async () => {
+    // remove all user input
     userInput = {};
+    // create user function will throw error
     await expect(Mutation.CreateUser(null, { user_input: userInput })).rejects.toThrowError('Username and password are required');
 
+    // declare expectation not called other database function because throwing error
     expect(mockUserModelFindOne).not.toHaveBeenCalled();
     expect(mockUserModelCreate).not.toHaveBeenCalled();
   });
 
-  it('throws error for invalid user type', async () => {
-    userInput.username = 'jest'
-    userInput.user_type = new ObjectId();
-    await expect(Mutation.CreateUser(null, { user_input: userInput })).rejects.toThrowError('Invalid user type');
-
-    expect(mockUserModelCreate).not.toHaveBeenCalled();
-  });
-
   it('Should throw error for existed username', async () => {
-    userInput.username = UserTestData.userData.username;
-    mockUserModelFindOne.mockImplementation(() => {
+    // user model find one return user object because existed user
+    mockUserModelFindOne = jest.spyOn(UserModel, 'findOne').mockImplementation(() => {
       return {
         lean: jest.fn().mockReturnValue(UserTestData.userData),
       };
     });
+    userInput.username = UserTestData.userData.username;
+
+    // call create user function with existed username then throwing error
     await expect(Mutation.CreateUser(null, { user_input: userInput })).rejects.toThrowError('Username already existed');
 
+    // declare expectation after throw error
+    expect(mockUserModelCreate).not.toHaveBeenCalled();
+  });
+
+  it('Should throw error for invalid user type', async () => {
+    // user model find one return null because no existed user
+    mockUserModelFindOne.mockImplementation(() => {
+      return {
+        lean: jest.fn().mockResolvedValue(null)
+      }
+    });
+    // find user type once
+    mockUserTypeModelFindOne.mockImplementationOnce();
+
+    // uodate user_type id
+    userInput.user_type = new ObjectId();
+
+    // call create user function with new user type id then throw error
+    await expect(Mutation.CreateUser(null, { user_input: userInput })).rejects.toThrowError('Invalid user type');
+
+    // declare expectation before and after throw error
+    expect(mockUserModelFindOne).toHaveBeenCalledTimes(1);
+    expect(mockUserTypeModelFindOne).toHaveBeenCalledTimes(1);
     expect(mockUserModelCreate).not.toHaveBeenCalled();
   });
 });
