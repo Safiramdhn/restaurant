89f0e2ad00c6395cb362dd833afe4279215f706c
Dewi Safira Ramadhani <safiramadhani9@gmail.com>
2024-05-29 15:13:21 +0800

    Jest configuration

---
+++ jest.config.js
@@ -0,0 +1,92 @@
+module.exports = {
+    // A set of glob patterns indicating which files should be considered test files (defaults to ["**/*.test.js"])
+    testMatch: ['**/__tests__/**/*.+(js|jsx|ts|tsx)', '**/?(*.)+(spec|test).+(js|jsx|ts|tsx)'],
+    testTimeout: 20000
+  
+    // A path to a module that sets up the test environment on a per-test basis (optional)
+    // setupFiles: ['<rootDir>/setupTests.js'],
+  
+    // A list of paths to modules that run before running the tests (optional)
+    // setupFilesAfterEnv: ['<rootDir>/setupAfterEnv.js'],
+  
+    // An object that configures Jest's module loader (optional)
+    // moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  
+    // A list of paths to directories that Jest should treat as containing test files (optional)
+    // testPathDirs: ['<rootDir>/__tests__'],
+  
+    // The test environment to use for testing (defaults to 'jsdom')
+    // testEnvironment: 'jsdom',
+  
+    // An array of regexp patterns or strings that Jest should use to identify files that should be skept from testing (optional)
+    // transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  
+    // An array of file extensions your test runner should support or transpile (optional)
+    // transform: {
+    //   '^.+\\.(js|jsx)<span class="math-inline">'\: '<rootDir\>/node\_modules/babel\-jest',
+  // \},
+  // An array of regexp patterns or strings that Jest should use to identify files that should be transformed \(optional\)
+  // testRegex\: \['/\.\*\\\\\.\(test\|spec\)\\\\\.\(js\|ts\)</span>'],
+  
+    // An object that configures Jasmine asynchronously (optional)
+    // jasmine: {},
+  
+    // An array of regexp patterns or strings that Jest should use to identify files that should be collected (optional)
+    // collectCoverageFrom: ['<rootDir>/src/**/*.{js,jsx,ts,tsx}'],
+  
+    // The root directory that Jest should use to resolve relative paths from config files (optional)
+    // rootDir: undefined,
+  
+    // A list of paths to additional modules Jest should load before executing the test suite (optional)
+    // moduleLoaderOptions: {
+    //   resolver: 'jest-resolver-node-modules',
+    // },
+  
+    // The glob patterns Jest uses to detect test files (optional)
+    // testRegex: undefined,
+  
+    // An array of file extensions your test runner should support or transpile (optional)
+    // transform: undefined,
+  
+    // An array of regexp patterns or strings that Jest should use to identify files that should be transformed (optional)
+    // testRegex: undefined,
+  
+    // A boolean value that indicates if coverage information should be collected (optional)
+    // collectCoverage: false,
+  
+    // An object that configures collectCoverage (optional)
+    // collectCoverageOptions: {},
+  
+    // An array of regexp patterns or strings that Jest should use to identify files that should be collected for coverage reports (optional)
+    // collectCoverageFrom: undefined,
+  
+    // The directory where Jest should store coverage output (optional)
+    // coverageDirectory: '<rootDir>/coverage',
+  
+    // An array of regexp patterns or strings that Jest should use to identify files that should be excluded from coverage reports (optional)
+    // coverageReporters: ['lcov', 'json'],
+  
+    // An array of file extensions your test runner should support or transpile (optional)
+    // transform: undefined,
+  
+    // An array of regexp patterns or strings that Jest should use to identify files that should be transformed (optional)
+    // testRegex: undefined,
+  
+    // A boolean value that indicates if the verbose flag will be passed to Jest (optional)
+    // verbose: false,
+  
+    // An array of regexp patterns or strings that Jest should use to identify files that should be shown in the watch mode terminal output (optional)
+    // watchPathIgnorePatterns: ['/node_modules/'],
+  
+    // An object that configures how Jest prints test results (optional)
+    // reporters: ['default'],
+  
+    // An boolean value that indicates whether Jest should run tests in parallel (optional)
+    // parallelizeTests: false,
+  
+    // The number of test workers that should be spawned in a worker farm. Defaults to the number of available CPUs minus 1 (optional)
+    // maxWorkers: undefined,
+  
+    // The path to a module that sets up the test environment on a per-global setup basis (optional)
+    // globalSetup: '<
+}  
